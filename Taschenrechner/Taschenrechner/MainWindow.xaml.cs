c5ec965bc8d757362bcdcacb385e1ab353336a86
cypherRene <cyberrene11@gmail.com>
2025-02-25 08:58:43 +0100

    Projekt Start Taschenrechner
    Projekt angelegt, Window Design und Addition

---
+++ Taschenrechner/Taschenrechner/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Taschenrechner;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Prozessor prozessor = new Prozessor();
+        int ergebnis = prozessor.add(2, 5);
+        remove();
+        MessageBox.Show(ergebnis.ToString());
+
+    }
+
+    public int remove()
+    {
+        return 0;
+    }
+}
---
+++ Taschenrechner/Taschenrechner/Prozessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class Prozessor
+    {
+        public int add(int zahl,int zahl2)
+        {
+            return zahl + zahl2;
+        }
+    }
+}
